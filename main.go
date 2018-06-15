dac2b3d3552c090b9d2a18baed89de02f00dd52b
Rohan Kanade <r0h4n@users.noreply.github.com>
2018-06-15 16:01:10 +0530

    Update main.go

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func newJob(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "{\"job_id\":\""+job_id+"\"}")
 }
 
-func getJobs(w http.ResponseWriter, r *http.Request) {
+func getJob(w http.ResponseWriter, r *http.Request) {
 	dirname := "/var/log/ansible/hosts/"
 	f, err := os.Open(dirname)
 	if err != nil {
@@ -48,7 +48,7 @@ func main() {
 	cluster_id := "cb012f6c-9cc1-4390-8d19-885dbf98dd4f"
 	mux := goji.NewMux()
 	mux.HandleFunc(pat.Post("/clusters/"+cluster_id+"/jobs"), newJob)
-	mux.HandleFunc(pat.Get("/clusters/"+cluster_id+"/jobs"), getJobs)
+	mux.HandleFunc(pat.Get("/clusters/"+cluster_id+"/jobs/:job_id"), getJob)
 	log.Println("listening")
 	go http.ListenAndServe("localhost:8000", mux)
 	if err := sds_sync.SyncAll(os.Getenv("GD2_ENDPOINT")); err != nil {
